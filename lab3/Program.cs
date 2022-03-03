6df332d65580c0929c61e714feabd03ae5fc9d26
Андрей Никоноров <andreynikonorov5@gmail.com>
2022-03-03 22:10:43 +0700

    Добавьте файлы проекта.

---
+++ lab3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using RomanEx;
+using RomanNumb;
+class Program
+{
+    static void Main(string[] arg)
+    {
+        try
+        {
+            RomanNumber number = new RomanNumber(3);
+            RomanNumber number2 = new RomanNumber(2);
+
+            Array myArr = Array.CreateInstance(typeof(RomanNumber), 10);
+            int lower = myArr.GetLowerBound(0), upper = myArr.GetUpperBound(0);
+            for (int i = lower; i <= upper; i++)
+            {
+                RomanNumber tmp = new RomanNumber((ushort)(i * 10 + 1));
+                myArr.SetValue(tmp, i);
+            }
+            myArr.SetValue(number, 3);
+
+            Print(myArr);
+
+            void Print(Array s)
+            {
+                Console.Write("RomanMassive : ");
+                for (int i = lower; i <= upper; i++)
+                {
+                    Console.Write($"{s.GetValue(i)} ");
+                }
+                Console.WriteLine("");
+            }
+
+            Array.Sort(myArr);
+            Print(myArr);
+
+            Console.WriteLine($"\nn1 - {number}, n2 - {number2}");
+             
+            Console.WriteLine($"Add(n1, n2) - {(number + number2).ToString()} ");
+
+            Console.WriteLine($"Sub(n1, n2) - {(number - number2).ToString()} ");
+
+            Console.WriteLine($"Mul(n1, n2) - {(number *number2).ToString()} ");
+          
+            Console.WriteLine($"Div(n1, n2) - {(number / number2).ToString()} ");
+
+        }
+        catch (RomanNumberException e)
+        {
+            Console.WriteLine($"{e.Message}");
+            Console.WriteLine($"Некорректное значение: {e.Value}");
+        }
+        catch (ArgumentNullException e)
+        {
+            Console.WriteLine($"{e.Message}");
+        }
+    }
+}
---
+++ lab3Test/RomanNumberTests.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RomanNumb;
+using RomanEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumb.Tests
+{
+    [TestClass()]
+    public class RomanNumberTests
+    {
+        [TestMethod()]
+        public void RomanNumberTest()
+        {
+            ushort n = 1;
+            ushort uncorrectValue = 0;
+            RomanNumber number = new RomanNumber(n);
+            RomanNumber? nullObject = null;
+
+            var value = number.Number == 1;
+            try
+            {
+                nullObject = new RomanNumber(uncorrectValue);
+            }
+
+            catch (RomanNumberException)
+            {
+                Assert.IsNull(nullObject);
+            }
+            Assert.IsTrue(value);
+            Assert.IsNotNull(number);
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            string[] values = {"XXV", "XL", "L", "LVII", "LXII"};
+            string?[] buffer = { "" , "", "", "", ""};
+            Array myArr = Array.CreateInstance(typeof(RomanNumber), 5);
+
+            int lower = myArr.GetLowerBound(0), upper = myArr.GetUpperBound(0);
+            for (int i = lower; i <= upper; i++)
+            {
+                RomanNumber tmp = new RomanNumber((ushort)((i + 1) * 100 / ((i + 1)+3)));
+                myArr.SetValue(tmp, i);
+                buffer[i] = myArr.GetValue(i).ToString();
+            }
+
+            for (int i = lower; i <= upper; i++)
+            {
+
+                Assert.AreEqual(values[i], buffer[i]); 
+            }
+            
+        }
+
+        [TestMethod()]
+        public void CloneTest()
+        {
+            RomanNumber firstValue = new RomanNumber(12);
+            RomanNumber secondValue;
+
+            secondValue = (RomanNumber)firstValue.Clone();
+
+            Assert.AreNotEqual(firstValue, secondValue);
+            Assert.AreEqual(firstValue.Number, secondValue.Number);
+        }
+
+        [TestMethod()]
+        public void CompareToTest()
+        {
+            ushort n = 1, value = 12;
+            RomanNumber number = new RomanNumber(n);
+            RomanNumber object1 = new RomanNumber(value);
+
+            var finalValue = number.CompareTo(object1);
+
+          
+            Assert.IsTrue(finalValue == -11);
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            ushort n = 1, value = 12;
+            RomanNumber number = new RomanNumber(n);
+            RomanNumber object1 = new RomanNumber(value);
+
+            number = number + object1;
+
+            Assert.AreEqual(number.Number, n + value);
+        }
+
+        [TestMethod()]
+        public void SubTest()
+        {
+            ushort n = 1, value = 12;
+            RomanNumber number = new RomanNumber(n);
+            RomanNumber object1 = new RomanNumber(value);
+            
+
+            RomanNumber finalValue = object1 - number;
+
+            /*Assert.ThrowsException<RomanNumberException>(() => RomanNumber.Sub(number, object1));*/
+            Assert.ThrowsException<OverflowException>(() => RomanNumber.Sub(number, object1));
+            Assert.AreEqual(finalValue.Number, value - n);
+        }
+
+        [TestMethod()]
+        public void DivTest()
+        {
+            ushort n = 1, value = 12;
+            RomanNumber number = new RomanNumber(n);
+            RomanNumber object1 = new RomanNumber(value);
+
+            RomanNumber newNumber = object1 / number;
+
+            Assert.ThrowsException<RomanNumberException>(() => RomanNumber.Div(number, object1));
+            Assert.AreEqual(newNumber.Number, value / n);
+        }
+
+        [TestMethod()]
+        public void MutTest()
+        {
+            ushort n = 1, value = 12, maxValue = 3999;
+            RomanNumber throwExceptionValue = new RomanNumber(maxValue);
+            RomanNumber number = new RomanNumber(n);
+            RomanNumber object1 = new RomanNumber(value);
+
+
+            number = number * object1;
+
+            Assert.AreEqual(number.Number, n * value);
+            Assert.ThrowsException<OverflowException>(() => RomanNumber.Mul(throwExceptionValue, throwExceptionValue));
+        }
+    }
+}
